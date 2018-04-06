2d0492ff4690efb513e0e0281dc17adfa5c57e39
Gypsy Rogers <grogers@nvidia.com>
2018-04-06 16:40:40 -0700

    now I can list all instances based on an attribute, so I can find all the instances with a stupid generic key

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # awsgypsy
+
+I just keep quicky scripts here, probably stuff no one else will care about 
---
+++ find-ec2-instance-by-attribute/find_instances_by.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+
+import sys
+
+if len(sys.argv) < 3:
+	print '''
+Usage: ./find_instances_by.py <attribute> <value>
+Example:
+   To find all instances with a key of MyKeyPair:
+   ./find_instnaces_by.py KeyName MyKeyPair
+'''
+	sys.exit(0) 
+
+
+
+findby = sys.argv[1]
+findval = sys.argv[2]
+
+import boto3    
+ec2client = boto3.client('ec2')
+
+
+
+
+
+from array import array
+conn = array('i')
+i = 1
+# Retrieves all regions/endpoints that work with EC2
+regions =  ec2client.describe_regions()
+for region in regions['Regions']:
+	regionname = region["RegionName"]
+	#based on this: https://www.slsmk.com/how-to-use-python-boto3-to-list-instances-in-amazon-aws/
+#	ec2client.setup_default_session(region_name=region["RegionName"])
+	boto3.Session(region_name=region["RegionName"])
+	conn = boto3.client('ec2',region_name=region["RegionName"]) #need to connect to each region individually
+	response = conn.describe_instances() #http://boto3.readthedocs.io/en/latest/reference/services/ec2.html#EC2.Client.describe_instances
+	#response = conn.describe_instances()
+	for reservation in response["Reservations"]:
+	    for instance in reservation["Instances"]:
+		if instance[findby] == findval:
+		# This sample print will output entire Dictionary object
+		#print(instance)
+		# This will print will output the value of the Dictionary key 'InstanceId'
+			print regionname,":",instance["Placement"]["AvailabilityZone"],instance["InstanceId"],":",findby,":",instance[findby]
